7cddc9c7ac6d8b3371bb2ab2154f66effae57a4d
Pascal Mayr <pascalmayr2@gmail.com>
2022-06-01 15:10:01 +0200

    redirect all undefined paths to home

--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -1,9 +1,13 @@
 import { Routes, Route } from 'react-router-dom'
 import Dashboard from 'pages/Dashboard'
+import Liquidation from 'pages/Liquidation'
+
 export default function CustomizeRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Dashboard />} />
+      <Route path="/liquidation" element={<Liquidation />} />
+      <Route path="*" element={<Dashboard />} />
     </Routes>
   )
 }
