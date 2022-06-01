a3edddd5f0314b9384a03a4b16bdb62ecbd88a52
Pascal Mayr <pascalmayr2@gmail.com>
2022-06-01 13:00:24 +0200

    fix typo

--- src/helpers/chainIcons.ts
+++ src/helpers/chainIcons.ts
@@ -1,6 +1,6 @@
 import BSCIcon from 'assets/images/svg/bsc_icon_logo.svg'
 import ETHIcon from 'assets/images/svg/ethereum-eth.svg'
-import PolyganIcon from 'assets/images/svg/polygon-matic-logo.svg'
+import PolygonIcon from 'assets/images/svg/polygon-matic-logo.svg'
 
 export default function chainIcons(name: string) {
   const _name = name.toLowerCase()
@@ -9,8 +9,8 @@ export default function chainIcons(name: string) {
       return BSCIcon
     case 'eth':
       return ETHIcon
-    case 'polygan':
-      return PolyganIcon
+    case 'polygon':
+      return PolygonIcon
     default:
       return ''
   }
