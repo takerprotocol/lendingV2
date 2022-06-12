f63484974c14dc45091f264fe839a109887d6ab4
Pascal Mayr <pascalmayr2@gmail.com>
2022-06-12 23:25:09 +0200

    add liquidate route

--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -2,12 +2,14 @@ import { Routes, Route } from 'react-router-dom'
 import Dashboard from 'pages/Dashboard'
 import Liquidation from 'pages/Liquidation'
 import Deposit from 'pages/Deposit'
+import Liquidate from 'pages/Liquidate'
 
 export default function CustomizeRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Dashboard />} />
       <Route path="/deposit" element={<Deposit />} />
+      <Route path="/liquidate" element={<Liquidate />} />
       <Route path="/liquidation" element={<Liquidation />} />
       <Route path="/liquidation/:page" element={<Liquidation />} />
       <Route path="*" element={<Dashboard />} />
