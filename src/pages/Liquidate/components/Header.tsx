bcaeb603c4e511a8822951a0fcb212b567c49de5
Pascal Mayr <pascalmayr2@gmail.com>
2022-06-13 15:59:41 +0200

    move header to external component

---
+++ src/pages/Liquidate/components/Header.tsx
@@ -0,0 +1,127 @@
+import Copy from 'components/Copy'
+import CollateralStat from './CollateralStat'
+import { styled } from '@mui/system'
+import { Typography } from '@mui/material'
+import { abbrevAddress } from 'utils/abbrevAddres'
+
+const CollateralStatsContainer = styled('div')`
+  display: flex;
+  gap: 10px;
+`
+
+const Stats = styled('div')`
+  background: #262338;
+  box-shadow: 0px 10px 20px rgba(38, 35, 56, 0.1);
+  border-radius: 12px;
+  padding: 24px;
+  display: flex;
+  flex-direction: column;
+`
+
+const CollateralAddress = styled(Typography)`
+  flex: 2;
+  font-family: 'Quicksand';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 24px;
+  line-height: 160%;
+  color: #a0a3bd;
+  display: flex;
+  gap: 10px;
+  align-items: center;
+
+  > div {
+    width: 24px !important;
+    height: 24px !important;
+  }
+
+  > div > svg {
+    width: 24px !important;
+    height: 24px !important;
+  }
+`
+
+const RiskLevelContainer = styled('div')`
+  flex: 1;
+  font-family: 'Quicksand';
+  font-style: normal;
+  font-weight: 600;
+  font-size: 14px;
+  line-height: 160%;
+  text-align: center;
+  text-transform: uppercase;
+
+  color: #e1536c;
+  display: flex;
+  justify-content: flex-end;
+`
+
+const RiskLevel = styled(Typography)`
+  border: 1px solid #e1536c;
+  border-radius: 12px;
+  padding-top: 2px;
+  padding-bottom: 2px;
+  padding-left: 10px;
+  padding-right: 10px;
+`
+
+const InfoContainer = styled('div')`
+  display: flex;
+  align-items: center;
+`
+
+type LiquidateHeaderType = {
+  address: string
+  riskPercentage: number
+  totalCollateral: number
+  nftCollateral: number
+  ethCollateral: number
+  totalDebt: number
+  ethDebt: number
+  borrowings: number
+}
+
+const LiquidateHeader = ({
+  address,
+  riskPercentage,
+  totalCollateral,
+  nftCollateral,
+  ethCollateral,
+  totalDebt,
+  ethDebt,
+  borrowings,
+}: LiquidateHeaderType) => {
+  return (
+    <Stats>
+      <InfoContainer>
+        <CollateralAddress>
+          {abbrevAddress(address)}
+          <Copy text={address} />
+        </CollateralAddress>
+        <RiskLevelContainer>
+          <RiskLevel>Risk level {riskPercentage}%</RiskLevel>
+        </RiskLevelContainer>
+      </InfoContainer>
+      <CollateralStatsContainer>
+        <CollateralStat
+          title="Collateral"
+          total={totalCollateral}
+          split1Title="NFT Collateral"
+          split1={nftCollateral}
+          split2Title="ETH Collateral"
+          split2={ethCollateral}
+        />
+        <CollateralStat
+          title="Total Debt"
+          total={totalDebt}
+          split1Title="ETH Debt"
+          split1={ethDebt}
+          split2Title="Borrowings"
+          split2={borrowings}
+        />
+      </CollateralStatsContainer>
+    </Stats>
+  )
+}
+
+export default LiquidateHeader
--- src/pages/Liquidate/index.tsx
+++ src/pages/Liquidate/index.tsx
@@ -1,12 +1,6 @@
-import { Box, Typography } from '@mui/material'
+import { Box } from '@mui/material'
 import { styled } from '@mui/system'
-import Copy from 'components/Copy'
-import CollateralStat from './components/CollateralStat'
-
-const CollateralStatsContainer = styled('div')`
-  display: flex;
-  gap: 10px;
-`
+import LiquidateHeader from './components/Header'
 
 const Body = styled(Box)`
   width: 100%;
@@ -15,99 +9,19 @@ const Body = styled(Box)`
   margin-top: 94px;
 `
 
-const Stats = styled('div')`
-  background: #262338;
-  box-shadow: 0px 10px 20px rgba(38, 35, 56, 0.1);
-  border-radius: 12px;
-  padding: 24px;
-  display: flex;
-  flex-direction: column;
-`
-
-const CollateralAddress = styled(Typography)`
-  flex: 2;
-  font-family: 'Quicksand';
-  font-style: normal;
-  font-weight: 700;
-  font-size: 24px;
-  line-height: 160%;
-  color: #a0a3bd;
-  display: flex;
-  gap: 10px;
-  align-items: center;
-
-  > div {
-    width: 24px !important;
-    height: 24px !important;
-  }
-
-  > div > svg {
-    width: 24px !important;
-    height: 24px !important;
-  }
-`
-
-const RiskLevelContainer = styled('div')`
-  flex: 1;
-  font-family: 'Quicksand';
-  font-style: normal;
-  font-weight: 600;
-  font-size: 14px;
-  line-height: 160%;
-  text-align: center;
-  text-transform: uppercase;
-
-  color: #e1536c;
-  display: flex;
-  justify-content: flex-end;
-`
-
-const RiskLevel = styled(Typography)`
-  border: 1px solid #e1536c;
-  border-radius: 12px;
-  padding-top: 2px;
-  padding-bottom: 2px;
-  padding-left: 10px;
-  padding-right: 10px;
-`
-
-const InfoContainer = styled('div')`
-  display: flex;
-  align-items: center;
-`
-
 const Liquidate = () => {
   return (
     <Body>
-      <Stats>
-        <InfoContainer>
-          <CollateralAddress>
-            1A12567J...36YU
-            <Copy text="1A12567J...36YU" />
-          </CollateralAddress>
-          <RiskLevelContainer>
-            <RiskLevel>Risk level 110%</RiskLevel>
-          </RiskLevelContainer>
-        </InfoContainer>
-        <CollateralStatsContainer>
-          <CollateralStat
-            title="Collateral"
-            total={526.1862}
-            split1Title="NFT Collateral"
-            split1={360.26}
-            split2Title="ETH Collateral"
-            split2={165.93}
-          />
-          <CollateralStat
-            title="Total Debt"
-            total={467.5814}
-            split1Title="ETH Debt"
-            split1={420.82}
-            split2Title="Borrowings"
-            split2={46.75}
-          />
-        </CollateralStatsContainer>
-      </Stats>
+      <LiquidateHeader
+        address="0x70997970C51812dc3A010C7d01b50e0d17dc79C8"
+        riskPercentage={110}
+        totalCollateral={526.1862}
+        nftCollateral={360.26}
+        ethCollateral={165.93}
+        totalDebt={467.5814}
+        ethDebt={420.82}
+        borrowings={46.75}
+      />
     </Body>
   )
 }
