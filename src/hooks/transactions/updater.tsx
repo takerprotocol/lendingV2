12af04e4dac13c5cfbedf628ad7f839ad53fc4a0
yourname <yourname@me.com>
2023-03-23 21:50:56 +0800

    fix ui

--- src/hooks/transactions/updater.tsx
+++ src/hooks/transactions/updater.tsx
@@ -35,6 +35,7 @@ const RETRY_OPTIONS_BY_CHAIN_ID: { [chainId: number]: RetryOptions } = {
   [SupportedChainId.ARBITRUM_RINKEBY]: { n: 10, minWait: 250, maxWait: 1000 },
   [SupportedChainId.OPTIMISTIC_KOVAN]: { n: 10, minWait: 250, maxWait: 1000 },
   [SupportedChainId.OPTIMISM]: { n: 10, minWait: 250, maxWait: 1000 },
+  [SupportedChainId.GOERLI]: { n: 10, minWait: 250, maxWait: 500 },
 }
 const DEFAULT_RETRY_OPTIONS: RetryOptions = { n: 1, minWait: 0, maxWait: 0 }
 
@@ -49,7 +50,6 @@ export default function Updater({ pendingTransactions, onCheck, onReceipt }: Upd
 
   const lastBlockNumber = useBlockNumber()
   const fastForwardBlockNumber = useFastForwardBlockNumber()
-
   const getReceipt = useCallback(
     (hash: string) => {
       if (!provider || !chainId) throw new Error('No provider or chainId')
